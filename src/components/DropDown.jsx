8d395c9aaf0f387b5144a4ca34c3c7f1a22ea619
Florian Bauer <florian@sbg.at>
2023-11-03 20:10:21 +0100

    fix

--- src/components/DropDown.jsx
+++ src/components/DropDown.jsx
@@ -28,7 +28,7 @@ function DropDown({ events, setNostrOpen }) {
         leave="transition ease-in duration-75"
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95">
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-10 mt-2 w-80 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           <div className="py-1 flex flex-col">
             <Menu.Item>
               {({ active }) => (
